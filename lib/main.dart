88920333d4d479e0e342e84ab79c427884755d12
Charlie Palmer <charlie.palmer@bluefrontier.co.uk>
2023-04-02 20:49:00 +0100

    chore: show github login and add window to front plugin functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:github/github.dart';
+import 'package:window_to_front/window_to_front.dart';    
+
 import 'github_oauth_credentials.dart';
 import 'src/github_login.dart';
 
@@ -31,16 +34,24 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return GithubLoginWidget(
       builder: (context, httpClient) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Text(title),
-            elevation: 2,
-          ),
-          body: const Center(
-            child: Text(
-              'You are logged in to GitHub!',
-            ),
-          ),
+        WindowToFront.activate();                       
+        return FutureBuilder<CurrentUser>(
+          future: viewerDetail(httpClient.credentials.accessToken),
+          builder: (context, snapshot) {
+            return Scaffold(
+              appBar: AppBar(
+                title: Text(title),
+                elevation: 2,
+              ),
+              body: Center(
+                child: Text(
+                  snapshot.hasData
+                      ? 'Hello ${snapshot.data!.login}!'
+                      : 'Retrieving viewer login details...',
+                ),
+              ),
+            );
+          },
         );
       },
       githubClientId: githubClientId,
@@ -48,4 +59,9 @@ class MyHomePage extends StatelessWidget {
       githubScopes: githubScopes,
     );
   }
+}
+
+Future<CurrentUser> viewerDetail(String accessToken) async {
+  final gitHub = GitHub(auth: Authentication.withToken(accessToken));
+  return gitHub.users.getCurrentUser();
 }
